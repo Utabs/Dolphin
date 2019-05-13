8cce48c09ec3e9cc9607f7c29d565201c00f921a
amin <aminrafiei1985@gmail.com>
2019-05-13 08:47:25 +0430

    changed User Entity

--- model/src/main/java/ir/dolphin/model/entity/security/Users.java
+++ model/src/main/java/ir/dolphin/model/entity/security/Users.java
@@ -2,122 +2,43 @@ package ir.dolphin.model.entity.security;
 
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Table(name = "user")
 public class Users {
 
 
-    public Users() {
-        this.id = id;
-        this.email = email;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.userName = userName;
-        this.password = password;
-        this.enable = enable;
-        this.roles = roles;
-    }
-
-    public Users(Users users) {
-        this.email = email;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.userName = userName;
-        this.password = password;
-        this.enable = enable;
-        this.roles = roles;
-    }
-
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id")
-    private int id;
-    @Column(name = "email")
-    private String email;
-    @Column(name = "first_name")
-    private String firstName;
-    @Column(name = "last_name")
-    private String lastName;
-    @Column(name = "username")
-    private String userName;
-    @Column(name = "password")
-    private String password;
-    @Column(name = "enable")
-    private int enable;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Id")
+    private Long id;
 
-//TODO: Change ManyToMany --> OneToMany
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(
-            name = "USER_AUTHORITY",
-            joinColumns = {@JoinColumn(name = "USER_ID", referencedColumnName = "ID")},
-            inverseJoinColumns = {@JoinColumn(name = "Role_ID", referencedColumnName = "ID")})
+    @Column(name = "Email", length = 80)
+    private String email;
+
+    @Column(name = "First_Name", length = 50
+            , columnDefinition = "NVARCHAR(50)")
+    private String firstName;
+
+    @Column(name = "Last_Name", length = 50
+            , columnDefinition = "NVARCHAR(50)")
+    private String lastName;
+
+    @Column(name = "UserName", length = 20)
+    @NotNull
+    private String userName;
+
+    @Column(name = "Password", length = 20)
+    @NotNull
+    private String password;
+
+    @Column(name = "Enable", columnDefinition = "int default 1")
+    private Integer enable;
+
+    @OneToMany
     private Set<Role> roles;
 
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public int getEnable() {
-        return enable;
-    }
-
-    public void setEnable(int enable) {
-        this.enable = enable;
-    }
-
-
-    public Set<Role> getRoles() {
-        return roles;
-    }
-
-    public void setRoles(Set<Role> roles) {
-        this.roles = roles;
-    }
 }
