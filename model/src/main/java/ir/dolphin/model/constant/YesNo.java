8c212b0f67dea238ad7edfadb0268a9241730a94
amin <aminrafiei1985@gmail.com>
2019-05-14 14:00:40 +0430

    ADD yes/no CONSTANT.

---
+++ model/src/main/java/ir/dolphin/model/constant/YesNo.java
@@ -0,0 +1,24 @@
+package ir.dolphin.model.constant;
+
+/**
+ *
+ * @author askari.abbas
+ */
+
+/**
+ *
+ * @ConstantName وضعيت بله و خير
+ */
+public class YesNo {
+    /**
+     *
+     * @ConstantAttribute خير
+     */
+    public final static Short NO = 0;
+
+    /**
+     *
+     * @ConstantAttribute بله
+     */
+    public final static Short YES = 1;
+}
