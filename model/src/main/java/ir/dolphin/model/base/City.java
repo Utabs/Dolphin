77aa7ef8d197040d72337772c2c719fa8e9afdc2
amin <aminrafiei1985@gmail.com>
2019-05-14 13:54:06 +0430

    ADD CITY AND PROVINCE ENTITY.

---
+++ model/src/main/java/ir/dolphin/model/base/City.java
@@ -0,0 +1,84 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ir.dolphin.model.base;
+
+
+/**
+ * @author Rafiee.Amin
+ */
+
+
+import ir.dolphin.model.BaseStaticEntity;
+
+import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+/**
+ * @EntityName شهرها
+ */
+
+
+@Entity
+@Table(name = "CITY", schema = "dbo")
+@NamedQueries({
+        @NamedQuery(name = "SallerProfile.findAll", query = "SELECT sallerProfile FROM SallerProfile sallerProfile")
+})
+
+public class City implements Serializable, BaseStaticEntity {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @AttributeName شناسه
+     */
+    @Id
+    @Column(name = "ID", nullable = false, unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    /**
+     * @AttributeName نام شهر
+     */
+    @Basic(optional = false)
+    @NotNull
+    @Size(max = 30)
+    @Column(name = "CITY_NAME", length = 30, nullable = false)
+    private String cityName;
+
+    /**
+     * @AttributeName نام انگلیسی
+     */
+    @Basic(optional = false)
+    @NotNull
+    @Size(max = 30)
+    @Column(name = "CITY_ENAME", length = 30, nullable = false)
+    private String cityEname;
+
+
+    /**
+     * @AttributeName وضعیت فعالیت
+     * @AttributeType YesNo
+     */
+    @Basic(optional = false)
+    @NotNull
+    @Min(value = 0)
+    @Max(value = 1)
+    @Column(name = "ACTIVE", nullable = false)
+    private Short active;
+
+    /**
+     * @AttributeName پیش شماره
+     */
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "PHONE_COD", nullable = false)
+    private String phoneCod;
+
+}
---
+++ model/src/main/java/ir/dolphin/model/base/Province.java
@@ -0,0 +1,73 @@
+package ir.dolphin.model.base;
+
+/**
+ * @author Rafiee.Amin
+ */
+
+
+import ir.dolphin.model.BaseStaticEntity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+/**
+ * @EntityName استانها
+ */
+
+
+@Entity
+@Table(name = "PROVINCE", schema = "dbo")
+@NamedQueries({
+        @NamedQuery(name = "SallerProfile.findAll", query = "SELECT sallerProfile FROM SallerProfile sallerProfile")
+})
+public class Province implements Serializable, BaseStaticEntity {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @AttributeName شناسه
+     */
+    @Id
+    @Column(name = "ID", nullable = false, unique = true)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    /**
+     * @AttributeName نام شهر
+     */
+    @Basic(optional = false)
+    @NotNull
+    @Size(max = 30)
+    @Column(name = "NAME", length = 30, nullable = false)
+    private String cityName;
+
+    /**
+     * @AttributeName نام انگلیسی شهر
+     */
+    @Basic(optional = false)
+    @NotNull
+    @Size(max = 30)
+    @Column(name = "ENAME", length = 30, nullable = false)
+    private String lastName;
+
+
+    /**
+     * @AttributeName تصویر
+     */
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "IMAGE", nullable = false)
+    private String image;
+
+    /**
+     * @AttributeName کد
+     */
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "POSTCOD", nullable = false)
+    private String postCod;
+
+
+}
