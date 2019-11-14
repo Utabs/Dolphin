311d5d8e2f87981776adcf0ecdc348fc46bf9de2
amin <aminrafiei@gmail.com>
2019-11-14 20:14:25 +0330

    - added swagger config

--- config/src/main/java/ir/dolphin/config/AppConfig.java
+++ config/src/main/java/ir/dolphin/config/AppConfig.java
@@ -11,6 +11,7 @@ import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -33,6 +34,14 @@ public class AppConfig extends WebMvcConfigurerAdapter {
 		}
 	}
 
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html")
+				.addResourceLocations("classpath:/META-INF/resources/");
+
+		registry.addResourceHandler("/webjars/**")
+				.addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
 //	@Override
 //	public void addCorsMappings(CorsRegistry registry) {
 //		registry.addMapping("/**");
