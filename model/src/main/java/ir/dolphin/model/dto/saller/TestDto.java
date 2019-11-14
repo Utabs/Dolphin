87f3535824b84c02739284532e0ef932579e7522
amin <aminrafiei@gmail.com>
2019-11-14 20:16:21 +0330

    - added models

---
+++ model/src/main/java/ir/dolphin/model/dto/saller/TestDto.java
@@ -0,0 +1,15 @@
+package ir.dolphin.model.dto.saller;
+
+import java.io.Serializable;
+
+public class TestDto implements Serializable {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
